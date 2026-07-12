fc5b70b54f0778acb0be987eedf7c49fc5d0b19a
Daniel Allen <dan@aeternagroup.net>
2026-07-12 21:57:48 +0100

    Update to schema version v4.80.0

--- main.tf
+++ main.tf
@@ -1,3 +1,8 @@
+data "azurerm_key_vault_secret" "protected_settings" {
+  for_each     = { for k, v in var.arc_machine_extensions : k => v if v.protected_settings_key_vault_id != null && v.protected_settings_key_vault_secret_name != null }
+  name         = each.value.protected_settings_key_vault_secret_name
+  key_vault_id = each.value.protected_settings_key_vault_id
+}
 resource "azurerm_arc_machine_extension" "arc_machine_extensions" {
   for_each = var.arc_machine_extensions
 
@@ -8,7 +13,7 @@ resource "azurerm_arc_machine_extension" "arc_machine_extensions" {
   type                      = each.value.type
   automatic_upgrade_enabled = each.value.automatic_upgrade_enabled
   force_update_tag          = each.value.force_update_tag
-  protected_settings        = each.value.protected_settings
+  protected_settings        = each.value.protected_settings != null ? each.value.protected_settings : try(data.azurerm_key_vault_secret.protected_settings[each.key].value, null)
   settings                  = each.value.settings
   tags                      = each.value.tags
   type_handler_version      = each.value.type_handler_version
